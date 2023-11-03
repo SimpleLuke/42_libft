1cbacf94ae78aca01fe17cb765d781fbd37b6263
Luke Lai <lukelai.dev@gmail.com>
2023-11-03 11:42:38 +0000

    Update ft_strncmp for comparing 0 byte

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -9,10 +9,21 @@
 /*   Updated: 2023/11/01 11:31:46 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stddef.h>
+#include "libft.h"
+
+/*
+ *  Description:
+ *  It compares the first n bytes between the strings s1 and s2.
+ *
+ *  Return value:
+ *  An integer greater than, equal to, or less than 0
+ *  if the string s1 is greater than, equal to, or less than the string s2.
+ */
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+  if (n == 0)
+    return (0);
 	while (*s1 != '\0' && *s1 == *s2 && n > 1)
 	{
 		s1++;
